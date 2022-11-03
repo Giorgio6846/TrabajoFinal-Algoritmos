2cdf686d57e1c927d54047c3d2a87080eadc186a
Giorgio Mancusi <6852733+Giorgio6846@users.noreply.github.com>
2022-11-03 13:55:28 -0500

    Added background_Game

---
+++ Juego.cpp
@@ -0,0 +1,8 @@
+#include "Juego.h"
+
+using namespace TrabajoFinal;
+
+int main()
+{
+	Application::Run(gcnew(TrabajoFinal::Juego));
+}
---
+++ Juego.h
@@ -0,0 +1,93 @@
+#pragma once
+
+namespace TrabajoFinal {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Juego
+	/// </summary>
+	public ref class Juego : public System::Windows::Forms::Form
+	{
+	public:
+		Juego(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Juego()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Timer^ TiempoRespuesta;
+	protected:
+
+	protected:
+	private: System::ComponentModel::IContainer^ components;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->TiempoRespuesta = (gcnew System::Windows::Forms::Timer(this->components));
+			this->SuspendLayout();
+			// 
+			// TiempoRespuesta
+			// 
+			this->TiempoRespuesta->Enabled = true;
+			this->TiempoRespuesta->Tick += gcnew System::EventHandler(this, &Juego::TiempoRespuesta_Tick);
+			// 
+			// Juego
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Name = L"Juego";
+			this->Text = L"Juego";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void TiempoRespuesta_Tick(System::Object^ sender, System::EventArgs^ e) {
+		Graphics^ gr = this -> CreateGraphics();
+		BufferedGraphicsContext^ bc = BufferedGraphicsManager::Current;
+
+		BufferedGraphics^ bg = bc->Allocate(gr, this->ClientRectangle);
+		bg->Graphics->Clear(Color::White);
+
+		Image^ image = Image::FromFile("Recursos\\Campo_de_concentracion.png");
+		Form::ClientSize = image->Size;
+		bg->Graphics->DrawImage(image, 0, 0, image->Size.Width, image->Size.Height);
+
+
+		bg->Render(gr);
+
+		delete bc, bg, gr;
+	}
+	};
+}
