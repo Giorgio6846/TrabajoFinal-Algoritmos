3079bdad55fef4df23f811b6bf003e4b738d9394
Giorgio Mancusi <6852733+Giorgio6846@users.noreply.github.com>
2022-11-05 15:02:32 -0500

    Changed Window Size

--- Juego.h
+++ Juego.h
@@ -193,7 +193,7 @@ namespace TrabajoFinal {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(960, 800);
+			this->ClientSize = System::Drawing::Size(1160, 800);
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"Juego";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
