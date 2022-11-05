bccdb302f5e536c0a9c8c7a8d1ab43431742f6f7
Drewks <u202211499@upc.edu.pe>
2022-11-05 11:55:26 -0500

    menu

--- MenuJuego.h
+++ MenuJuego.h
@@ -45,6 +45,9 @@ namespace TrabajoFinal {
 		/// </summary>
 		System::ComponentModel::Container ^components;
 	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button5;
 
 		   Graphics^ gr;
 
@@ -58,6 +61,9 @@ namespace TrabajoFinal {
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MenuJuego::typeid));
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
@@ -68,12 +74,12 @@ namespace TrabajoFinal {
 			this->button1->Font = (gcnew System::Drawing::Font(L"Maiandra GD", 26.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button1->ForeColor = System::Drawing::SystemColors::Control;
-			this->button1->Location = System::Drawing::Point(363, 230);
+			this->button1->Location = System::Drawing::Point(246, 171);
 			this->button1->Margin = System::Windows::Forms::Padding(0);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(185, 105);
+			this->button1->Size = System::Drawing::Size(399, 139);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"Play";
+			this->button1->Text = L"JUGAR";
 			this->button1->UseVisualStyleBackColor = false;
 			this->button1->Click += gcnew System::EventHandler(this, &MenuJuego::button1_Click);
 			// 
@@ -82,24 +88,73 @@ namespace TrabajoFinal {
 			this->button2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(192)),
 				static_cast<System::Int32>(static_cast<System::Byte>(192)));
 			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->button2->Font = (gcnew System::Drawing::Font(L"Maiandra GD", 26.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->button2->Font = (gcnew System::Drawing::Font(L"Maiandra GD", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button2->ForeColor = System::Drawing::SystemColors::Control;
 			this->button2->ImageAlign = System::Drawing::ContentAlignment::TopLeft;
-			this->button2->Location = System::Drawing::Point(363, 358);
+			this->button2->Location = System::Drawing::Point(463, 389);
 			this->button2->Margin = System::Windows::Forms::Padding(0);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(185, 105);
+			this->button2->Size = System::Drawing::Size(182, 43);
 			this->button2->TabIndex = 1;
-			this->button2->Text = L"Exit";
+			this->button2->Text = L"SALIR";
 			this->button2->UseVisualStyleBackColor = false;
 			this->button2->Click += gcnew System::EventHandler(this, &MenuJuego::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(128)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Font = (gcnew System::Drawing::Font(L"Maiandra GD", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button3->ForeColor = System::Drawing::SystemColors::Control;
+			this->button3->Location = System::Drawing::Point(463, 328);
+			this->button3->Margin = System::Windows::Forms::Padding(0);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(182, 43);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"DIFICULTAD";
+			this->button3->UseVisualStyleBackColor = false;
+			// 
+			// button4
+			// 
+			this->button4->BackColor = System::Drawing::Color::Red;
+			this->button4->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button4->Font = (gcnew System::Drawing::Font(L"Maiandra GD", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button4->ForeColor = System::Drawing::SystemColors::Control;
+			this->button4->Location = System::Drawing::Point(246, 328);
+			this->button4->Margin = System::Windows::Forms::Padding(0);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(182, 43);
+			this->button4->TabIndex = 3;
+			this->button4->Text = L"INSTRUCCIONES";
+			this->button4->UseVisualStyleBackColor = false;
+			// 
+			// button5
+			// 
+			this->button5->BackColor = System::Drawing::Color::Blue;
+			this->button5->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button5->Font = (gcnew System::Drawing::Font(L"Maiandra GD", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button5->ForeColor = System::Drawing::SystemColors::Control;
+			this->button5->Location = System::Drawing::Point(246, 389);
+			this->button5->Margin = System::Windows::Forms::Padding(0);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(182, 43);
+			this->button5->TabIndex = 4;
+			this->button5->Text = L"CRÉDITOS";
+			this->button5->UseVisualStyleBackColor = false;
 			// 
 			// MenuJuego
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(900, 600);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
@@ -130,5 +185,5 @@ namespace TrabajoFinal {
 	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
 		exit(0);
 	}
-	};
+};
 }
