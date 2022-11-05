a9ca6ad81e1d249aaac6b0a58815a5332883eddb
Drewks <u202211499@upc.edu.pe>
2022-11-05 18:04:39 -0500

    menuuu

--- MenuJuego.h
+++ MenuJuego.h
@@ -2,6 +2,7 @@
 #include "Juego.h"
 #include "Instrucciones.h"
 #include "Creditos.h"
+#include "Dificultad.h"
 namespace TrabajoFinal {
 
 	using namespace System;
@@ -77,11 +78,11 @@ namespace TrabajoFinal {
 				static_cast<System::Byte>(0)));
 			this->button2->ForeColor = System::Drawing::SystemColors::Control;
 			this->button2->ImageAlign = System::Drawing::ContentAlignment::TopLeft;
-			this->button2->Location = System::Drawing::Point(463, 389);
+			this->button2->Location = System::Drawing::Point(463, 385);
 			this->button2->Margin = System::Windows::Forms::Padding(0);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(182, 43);
-			this->button2->TabIndex = 1;
+			this->button2->TabIndex = 5;
 			this->button2->Text = L"SALIR";
 			this->button2->UseVisualStyleBackColor = false;
 			this->button2->Click += gcnew System::EventHandler(this, &MenuJuego::button2_Click);
@@ -98,7 +99,7 @@ namespace TrabajoFinal {
 			this->button3->Margin = System::Windows::Forms::Padding(0);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(182, 43);
-			this->button3->TabIndex = 2;
+			this->button3->TabIndex = 4;
 			this->button3->Text = L"DIFICULTAD";
 			this->button3->UseVisualStyleBackColor = false;
 			this->button3->Click += gcnew System::EventHandler(this, &MenuJuego::button3_Click);
@@ -114,7 +115,7 @@ namespace TrabajoFinal {
 			this->button4->Margin = System::Windows::Forms::Padding(0);
 			this->button4->Name = L"button4";
 			this->button4->Size = System::Drawing::Size(182, 43);
-			this->button4->TabIndex = 3;
+			this->button4->TabIndex = 2;
 			this->button4->Text = L"INSTRUCCIONES";
 			this->button4->UseVisualStyleBackColor = false;
 			this->button4->Click += gcnew System::EventHandler(this, &MenuJuego::button4_Click);
@@ -126,11 +127,11 @@ namespace TrabajoFinal {
 			this->button5->Font = (gcnew System::Drawing::Font(L"Maiandra GD", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button5->ForeColor = System::Drawing::SystemColors::Control;
-			this->button5->Location = System::Drawing::Point(246, 389);
+			this->button5->Location = System::Drawing::Point(246, 385);
 			this->button5->Margin = System::Windows::Forms::Padding(0);
 			this->button5->Name = L"button5";
 			this->button5->Size = System::Drawing::Size(182, 43);
-			this->button5->TabIndex = 4;
+			this->button5->TabIndex = 3;
 			this->button5->Text = L"CRÉDITOS";
 			this->button5->UseVisualStyleBackColor = false;
 			this->button5->Click += gcnew System::EventHandler(this, &MenuJuego::button5_Click);
@@ -147,7 +148,7 @@ namespace TrabajoFinal {
 			this->button1->Margin = System::Windows::Forms::Padding(0);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(399, 139);
-			this->button1->TabIndex = 0;
+			this->button1->TabIndex = 1;
 			this->button1->Text = L"JUGAR";
 			this->button1->UseVisualStyleBackColor = false;
 			this->button1->Click += gcnew System::EventHandler(this, &MenuJuego::button1_Click);
@@ -191,9 +192,8 @@ namespace TrabajoFinal {
 		exit(0);
 	}
 private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
-	Juego^ juego = gcnew Juego();
-	juego->setDificultad('M');
-	juego->Show();
+	Dificultad^ dificultad = gcnew Dificultad();
+	dificultad->Show();
 }
 private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
 	Intrucciones^ intrucciones = gcnew Intrucciones();
